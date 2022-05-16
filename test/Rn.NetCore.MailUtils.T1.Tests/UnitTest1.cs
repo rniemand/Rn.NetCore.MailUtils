24a60dd065b7deb438acd3a02e7d5d624a82fef3
Richard Niemand <niemand.richard@gmail.com>
2022-05-16 17:39:49 -0600

    Stubbing tests

---
+++ test/Rn.NetCore.MailUtils.T1.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Rn.NetCore.MailUtils.T1.Tests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
